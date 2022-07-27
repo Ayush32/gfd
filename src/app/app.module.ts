f56d1cf97caeeb852dbf34e823d6d06e0f880b50
Ayush Gupta <721ayush@gmail.com>
2022-07-27 12:37:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
